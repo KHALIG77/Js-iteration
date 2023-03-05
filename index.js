059cca2cff4e8deb58ee61b937c2596380b36a24
Khalig <khaligfm@code.edu.az>
2023-03-05 19:55:59 +0400

    Iteration

---
+++ index.js
@@ -0,0 +1,177 @@
+
+                                                             // ITERATION SOME START
+
+// const someArray = [1, 2, 3,0, 5, 6, 7];
+
+
+// mySomeFunction(someArray,myFunction)
+
+// function myFunction(item){
+//     return item > 18
+
+
+// }
+// console.log(myFunction)
+// function mySomeFunction(array,callback){
+//     for (let index = 0; index < array.length; index++) {
+//         if(callback(array[index])){ 
+//             return  true
+//         }   
+//     }
+//     return false
+
+
+// }
+
+
+// console.log(mySomeFunction(someArray,myFunction))
+
+
+
+
+                                                                   //  ITERATION SOME END
+
+                                                                    // ITERATION EVERY START
+
+// const everyArray = [1,2,3,4,5]
+
+// // 
+
+
+//  function myEveryFunction(item){
+//    return item > 2
+//  }
+// function myCallBackEvery(array,callback){
+//     for (let index = 0; index < array.length; index++) {
+//        if(!callback(array[index])){
+//             return false
+//        }
+//     }
+// return true 
+//  }
+//  console.log(myCallBackEvery(everyArray,myEveryFunction))
+ 
+                                                                  // ITERATION EVERY END
+
+                                                                  //ITERATION FOREACH START
+
+// const arrForEach = [5,7,8,9]
+// myForEach(arrForEach,callBack)
+
+// function callBack(item){
+//   console.log(item)
+// }
+// function myForEach(arr,callback){
+// for(let i = 0;i<arr.length;i++){
+//   callback(arr[i])
+// }
+// }
+                                                                   //ITERATION FOREACH END
+
+                                                                   //ITERATION MAP START
+
+
+// const arrForMap = [4,5,6,7,8]
+// let newArray = []
+
+
+// function callBack(item){
+
+//    newArray.push(item + 10)
+
+// }
+// function myMap(arr,callback){
+// for(let i = 0;i<arr.length;i++){
+ 
+//   callback(arr[i])
+
+// }
+//    return newArray
+
+// }
+// console.log(myMap(arrForMap,callBack))                                 
+//                                                               // ITERATION MAP END      
+                                                                 
+
+                                                                  // ITERATION FILTER START
+// const arrForFilter = [4,15,30,40]
+
+
+// function callBack(item){
+// return item > 10
+// }
+
+
+// function filter(arr,callback){
+//   let newArray = []
+// for(let i = 0;i<arr.length;i++){
+// if(callback(arr[i])){
+//   newArray.push(arr[i])
+// }
+// }
+// return newArray
+
+// }
+// console.log(filter(arrForFilter,callBack))
+                                                                            // ITERATION FILTER END
+                                                                              
+
+                                                                            // ITERATION INDEXOF START
+
+// const arrForIndexOf = [10,20,40,50]
+
+
+
+// function callBack(item){
+// return item===20
+// }
+
+
+// function arrForIndex(arr,callback){
+//   let indexOf
+// for(let i = 0;i<arr.length;i++){
+  
+//   if(callback(arr[i])){
+//     indexOf = i
+//     break;
+//   }
+//   if(arr[i]===arr[arr.length-1]){
+//     return -1
+//   }
+// }
+// return indexOf
+
+// }
+// console.log(arrForIndex(arrForIndexOf,callBack))                  
+                                                                       
+                                                                      // ITERATION INDEXOF END
+
+                                                                      // ITERATION FIND START
+
+// const arrFind = [20,40,50,70,90]
+//  let foundItem
+//  console.log(itemFound(arrFind,callBack))
+
+
+//  function itemFound(arr,callback){
+//   for(let i = 0;i<arr.length;i++){
+//     if(callback(arr[i])){
+//       foundItem = arr[i]
+//       return foundItem
+//     }
+
+//   } 
+//   return "Tapilmadi"
+
+//  }
+//  function callBack(item){
+//      return item > 100
+//  }
+                                                                //  ITERATION FIND END
+                                                                
+
+
+
+
+
+
